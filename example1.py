cd6622e936b5cc2c5de332cfcab8c5daebb432a6
miriam31 <zs16012137@estudiantes.uv.mx>
2020-02-27 15:51:16 -0600

    se hicieron ejemplos

--- example1.py
+++ example1.py
@@ -1,1 +1,50 @@
 print('hello')
+
+#nombre: tipo=inicializacion
+
+number_int: int=8 #con inicializacion
+number2_int: int
+
+number2_int=9
+
+number_float: float=3.8
+
+result: int= number_int + int(number_float)
+flag2: bool = False
+flag: bool = True
+text: str = 'mensajito'
+
+msj = input('give me a little number')
+number_in: float = float(msj)
+
+#str() chr()
+
+r: range = range(6,20,3)
+
+# + - * / **  // %  operadores aritmeticos
+
+# < >  <= >= == != operadores relacionales
+
+#  & and  |  or  not operadores logicos
+
+ch1: str = 'A'
+ch2: str = 'c'
+
+num_float: float= float (input('give me a number'))
+if num_float >= 0:
+    print(num_float)
+
+else:
+    print(-num_float)
+
+
+n: int=8
+r: range = range (0,n,1)
+word: str = 'pepe'
+for _ in word:
+    print('hola')
+
+n: int=28
+pal: str = 'esta linea es hermosa'
+
+
